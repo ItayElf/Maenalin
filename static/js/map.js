614962f7800b631316ac07efc7a5466454fbe968
ItayElf <thh123321@gmail.com>
2023-07-01 15:20:19 +0300

    Feat: added base interactive map

---
+++ static/js/map.js
@@ -0,0 +1,14 @@
+var map = L.map("map", {
+  crs: L.CRS.Simple,
+  minZoom: -2,
+});
+
+var bounds = [
+  [0, 0],
+  [3584, 4864],
+];
+var image = L.imageOverlay("images/maenalin_background.png", bounds).addTo(map);
+
+map.fitBounds(bounds);
+map.setMaxBounds(map.getBounds());
+map.setZoom(-1);
