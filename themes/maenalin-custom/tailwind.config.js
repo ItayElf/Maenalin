e6bf202224a20394d10982d16cc733810dea37ea
איתי בן חיים <thh123321@gmail.com>
2025-06-30 23:48:18 +0300

    Fix: css

--- themes/maenalin-custom/tailwind.config.js
+++ themes/maenalin-custom/tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["content/**/*.md", "layouts/**/*.html", "../../content/**/*.md"],
   theme: {
     extend: {
+      gridTemplateColumns: {
+        '12': 'repeat(12, minmax(0, 1fr))',
+        '8': 'repeat(8, minmax(0, 1fr))',
+      },
       colors: {
         primary: {
           DEFAULT: "#a20021",
